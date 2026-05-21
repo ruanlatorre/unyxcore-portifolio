5f2ed317b9d514fff82b29cb20fa5af694303b52
Rafael Adriano <rafael.adriano2801@gmail.com>
2026-05-21 16:40:19 -0300

    Tentativa de ajsutes finais para a pagina de admin ser acessada pelo dominio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Essencial para rotas aninhadas funcionarem (ex: /admin/cadastrar-venda)
   build: {
     sourcemap: false,
+    outDir: 'dist', // Diretório padrão do Vite que o Vercel vai ler
+  },
+  server: {
+    port: 5173,
   }
 })
